d90bfcf0e68e003629d59aef0296b78dbb176b9d
Феденко Олег Алексеевич <o.fedenko@2gis.ru>
2023-05-29 15:57:24 +0300

    3

--- task2.java
+++ task2.java
@@ -4,19 +4,17 @@ package Seminar1;
 
 public class task2 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 1001; i++) {
-            // поставили флаг
-            boolean isPrime = true;  
-            for (int j = 2; j <= Math.floor(Math.sqrt(i)); j++){
-                //Если j - делитель, флаг = false
-                if ((i % j) == 0) { 
-                    isPrime = false;
-                    break;
-                }
+        System.out.println("Простые числа от 1 до 100: ");
+        for(int i = 2; i <= 1001; i++) {
+            int count = 0;
+            for(int j = 1; j < i; j++) {
+                if (i % j == 0) count++;
             }
-            if (isPrime){
-                System.out.print(i + " "); //если делителей нет, флаг остался True, число на печать
+            if (count == 1){
+                System.out.printf("%d, ", i);
             }
         }
+        
     }
+    
 }
